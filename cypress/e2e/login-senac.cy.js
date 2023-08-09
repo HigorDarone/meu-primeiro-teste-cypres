2d36ef5cbb03d275138d4c303cb04eec891628e4
HigorDarone <higordarone@hotmail.com>
2023-08-09 20:21:24 -0300

    primeiro teste em cypress

---
+++ cypress/e2e/login-senac.cy.js
@@ -0,0 +1,18 @@
+
+
+describe('template spec', () => {
+  it('passes', () => {
+    cy.visit('https://www.sp.senac.br/')
+
+    cy.get('.mburger').click()
+
+    cy.get('#txtLoginNaoLogadoMob').click()
+
+    cy.get('#login-email').type('higordarone@hotmail.com')
+
+    cy.get('#login-password').type('hd12389')
+
+    cy.get('#btnLoginHeader').click()
+    
+  })
+})
